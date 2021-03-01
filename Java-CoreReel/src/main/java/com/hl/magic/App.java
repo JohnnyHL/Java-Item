2f19e3d160b593b941d093f78a3459318c47ee30
JohnnyHL <316839314@qq.com>
2021-03-01 21:01:07 +0800

    Java核心技术卷1和卷2

---
+++ Java-CoreReel/src/main/java/com/hl/magic/App.java
@@ -0,0 +1,13 @@
+package com.hl.magic;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
